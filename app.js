2bf7236881780acaa02891dc58b55450584e0c19
Mohamed Elkarim <mohdawad2010@hotmail.com>
2018-12-02 20:14:58 -0500

    completed logic

--- app.js
+++ app.js
@@ -14,26 +14,44 @@ const port = 3000;
 server.listen(port);
 console.log('App listening at port: ' + port);
 
-
+let dresses = [{
+        name: 'yellow dress',
+        image: 'https://connaisseurparis.com/wp-content/uploads/2018/08/D61A7128-291D-43D6-9809-8ED7E3FB07A1-682x1024-266x400.jpeg',
+        description: 'no sure dress',
+        rating: 3
+      }]
 io.on('connection', client => {
   console.log('Client connected...' + client);
+  client.emit('Welcome', 'hello')
 
   client.on('Dress', data => {
-    console.log(data);
-    client.broadcast.emit('Dress', data)
+    console.log(dresses);
+    if(data != dresses[dresses.length - 1]){
+      client.broadcast.emit('Dress', data)
+    }
+  })
+
+  client.on('subscribeToDress', (interval) => {
+    console.log('client is subscribing to timer with interval ', interval);
+    client.emit('dress', dresses)
+  });
+
+
+  client.on('Next Dress', data => {
+    client.broadcast.emit('Next Dress', data)
   })
 
   client.on('Previous Dress', data => {
     client.broadcast.emit('Previous Dress', data)
   })
 
-  client.on('Next Dress', data => {
-    client.broadcast.emit('Next Dress', data)
+  client.on('Reset', data => {
+    dresses = []
+    client.broadcast.emit(dresses)
   })
 
-  client.on('messages', data => {
-    client.emit('broad', data);
-    client.broadcast.emit('broad', data);
+  client.on('Reset to Point', data => {
+    client.broadcast.emit(dresses.splice(data))
   })
 
 })
