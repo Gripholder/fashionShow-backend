46eed7c7eaa9794481bdf143638ecae93af2e1d1
Mohamed Elkarim <mohdawad2010@hotmail.com>
2018-12-07 11:22:20 -0500

    added rating functionality

--- app.js
+++ app.js
@@ -15,21 +15,61 @@ server.listen(port);
 console.log('App listening at port: ' + port);
 
 let dresses = [{
+        id: 0,
         name: 'yellow dress',
         image: 'https://connaisseurparis.com/wp-content/uploads/2018/08/D61A7128-291D-43D6-9809-8ED7E3FB07A1-682x1024-266x400.jpeg',
-        description: 'no sure dress',
-        rating: 3
+        description: 'no sure dress'
       }]
+
+let ratingList = [[]]
+
+const averageRating = id => {
+  console.log('idily' + id)
+  let result = 0;
+  for (let i = 0; i < ratingList[id].length; i++) {
+    result += ratingList[id][i]
+  }
+  result = result / ratingList[id].length
+  console.log('average Rating ' + result)
+  return Math.round(result)
+}
+
 io.on('connection', client => {
   console.log('Client connected...' + client);
   client.emit('Welcome', 'hello')
 
   client.on('Dress', data => {
-    console.log(dresses);
+    console.log('creating rating')
+    console.log(ratingList);
+    ratingList.push([])
+    console.log(ratingList);
     if(data != dresses[dresses.length - 1]){
       client.broadcast.emit('Dress', data)
     }
+    dresses.push(data)
   })
+
+  client.on('Get Dresses', data => {
+    client.emit('Current Dresses', dresses)
+  })
+
+  client.on('Submit Rating', data => {
+    ratingList[data[0]].push(data[1])
+    let rate = averageRating(data[0])
+    client.broadcast.emit('Rating', rate)
+    client.emit('Rating', rate)
+  })
+
+  client.on('Get Rating', id => {
+    console.log('sending rating' + id)
+    console.log('getting rating average ' + averageRating(id))
+    if(averageRating(id) == NaN){
+      client.emit('Rating', 0)
+    } else {
+      client.emit('Rating', averageRating(id))
+    }
+  })
+
 
   client.on('subscribeToDress', (interval) => {
     console.log('client is subscribing to timer with interval ', interval);
